5e4873ac0e55aaf3d6cf179c11542c778c36ea78
Citronskal <yiaswift@gmail.com>
2024-05-25 10:15:24 +0200

    changed photos

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -9,30 +9,22 @@ const imageStyle = {
 export const HomePage = () => (
   <Carousel autoplay>
     <div>
+      <img src="/photo1.jpg" alt="Slide 1" style={imageStyle} />
+    </div>
+    <div>
+      <img src="/photo2.jpg" alt="Slide 2" style={imageStyle} />
+    </div>
+    <div>
       <img
-        src="https://images.unsplash.com/photo-1519671282429-b44660ead0a7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Slide 1"
+        src="/photo3.jpg"
+        alt="Slide 3"
         style={imageStyle}
       />
     </div>
     <div>
       <img
-        src="https://plus.unsplash.com/premium_photo-1678914045640-55a120a8f849?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Slide 1"
-        style={imageStyle}
-      />
-    </div>
-    <div>
-      <img
-        src="https://plus.unsplash.com/premium_photo-1681830684950-54e3adbc9f2d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Slide 1"
-        style={imageStyle}
-      />
-    </div>
-    <div>
-      <img
-        src="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Slide 1"
+        src="/photo4.jpg"
+        alt="Slide 4"
         style={imageStyle}
       />
     </div>
