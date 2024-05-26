3f180311c97ed5c2cd97f75904f69b054e8e34cd
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-05-26 11:47:42 +0200

    Update - Made /register async and adj res to only ID + Added token to /login res

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import mongoose, { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
-import expressListEndpoints from "express-list-endpoints"
+import expressListEndpoints from "express-list-endpoints";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl);
@@ -12,7 +12,6 @@ const userSchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
   },
   password: {
     type: String,
@@ -21,7 +20,7 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   token: {
     type: String,
@@ -47,16 +46,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // Start defining your routes here
 app.get("/", (req, res) => {
   const endpoints = expressListEndpoints(app);
-  res.json(endpoints)
-})
-
+  res.json(endpoints);
+});
 
 // Register new user
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { name, phone, email, password } = req.body;
     const salt = bcrypt.genSaltSync();
@@ -67,25 +64,31 @@ app.post("/register", (req, res) => {
       password: bcrypt.hashSync(password, salt),
     });
     user.save();
-    res.status(201).send(user);
+    res.status(201).send(`UserID ${user._id} successfully created!`);
   } catch (err) {
     res.status(400).json({
-      message: "Could not create user",
+      message: "Could not create user.",
       errors: err.errors,
     });
   }
 });
+
 // Login
 app.post("/login", async (req, res) => {
   const matchUser = await User.findOne({
     email: req.body.email,
   });
   if (matchUser && bcrypt.compareSync(req.body.password, matchUser.password)) {
-    res.json({ matchUserId: matchUser._id });
+    res.json({
+      message: "User logged in.",
+      matchUserId: matchUser._id,
+      token: matchUser.token,
+    });
   } else {
     res.json({ message: "User not found." });
   }
 });
+
 // Authorize
 const authenticateUser = async (req, res, next) => {
   const user = await User.findOne({
@@ -100,12 +103,14 @@ const authenticateUser = async (req, res, next) => {
     });
   }
 };
+
 app.get("/secrets", authenticateUser);
 app.get("/secrets", (req, res) => {
   res.json({
     secret: "This is secrets.",
   });
 });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
